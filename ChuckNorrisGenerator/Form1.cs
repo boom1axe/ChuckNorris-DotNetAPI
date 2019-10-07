18a774f77568be2bc1da179da1908534089465cf
Dennis Kafton <boom1axe@att.net>
2019-10-07 15:46:25 -0700

    Added ChuckNorrisGenerator Form and populated combo box cboCatagoies.

---
+++ ChuckNorrisGenerator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuckNorrisGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            populateForm();
+        }
+
+        private async void populateForm()
+        {
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var cat in categories)
+            {
+                cboCatagoies.Items.Add(cat);
+            }
+        }
+    }
+}
